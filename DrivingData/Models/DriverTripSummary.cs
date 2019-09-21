cb02f11995a74ad91701682ca348639c9ff2d96f
MAPDOM\bdavis <bdavis@mapsysinc.com>
2019-09-21 16:33:55 -0400

    Rudimentary report. I wanted to write at least a stub of this to get the data structure right before I made a test that wasn't useful.

---
+++ DrivingData/Models/DriverTripSummary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrivingData.Models
+{
+    class DriverTripSummary
+    {
+        public string DriverName { get; set; }
+        public decimal TotalDistance { get; set; }
+        public int TotalMinutes { get; set; } // I used int here because times are always given in whole minutes already
+
+    }
+}
---
+++ DrivingData/Services.cs
@@ -0,0 +1,43 @@
+﻿using DrivingData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DrivingData
+{
+    // So this isn't a real web API but if it were these would be service endpoints
+    class Services
+    {
+        public string GenerateReport(List<Trip> trips)
+        {
+            //TODO discard < 5 and > 100
+
+            //Aggregate everything
+            var driverTripSummaries = trips.GroupBy(t => t.DriverName).Select(g => new DriverTripSummary()
+            {
+                DriverName = g.Key,
+                TotalDistance = g.Sum(t => t.MilesDriven),
+                TotalMinutes = Convert.ToInt32(g.Sum(t => t.EndTime.Subtract(t.StartTime).TotalMinutes)) // convert the sum rather than each item
+            });
+            
+            //Generate Report
+            //TODO sort the list
+            var sb = new StringBuilder();
+            foreach (var summary in driverTripSummaries)
+            {
+                // Problem specified "to nearest int" so I'm using Convert.ToInt32 because it rounds and converts double to int in one step.
+                // If this were real life, you better believe I'd be asking someone who cared which way to round these. 
+                // Manual says:
+                // "rounded to the nearest 32-bit signed integer. If value is halfway between two whole numbers, 
+                // the even number is returned; that is, 4.5 is converted to 4, and 5.5 is converted to 6."
+                var miles = Convert.ToInt32(summary.TotalDistance);
+                var mph = Convert.ToInt32(summary.TotalDistance / summary.TotalMinutes));
+
+                sb.AppendFormat("{0}: {1} miles @ {2} mph", summary.DriverName, miles, mph);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
