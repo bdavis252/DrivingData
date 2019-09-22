f6553ce90b76542003be9fb928c43b32347f5a26
MAPDOM\bdavis <bdavis@mapsysinc.com>
2019-09-21 21:17:32 -0400

    Had to go to a birthday party for my son's friend. Added list sorting. New lines on all but the last. Turns out miles per hour is not miles per minute.

--- DrivingData/Services.cs
+++ DrivingData/Services.cs
@@ -19,22 +19,28 @@ namespace DrivingData
                 DriverName = g.Key,
                 TotalDistance = g.Sum(t => t.MilesDriven),
                 TotalMinutes = Convert.ToInt32(g.Sum(t => t.EndTime.Subtract(t.StartTime).TotalMinutes)) // convert the sum rather than each item
-            });
+            }).OrderByDescending(dts => dts.TotalDistance);
             
             //Generate Report
-            //TODO sort the list
             var sb = new StringBuilder();
-            foreach (var summary in driverTripSummaries)
+            int count = driverTripSummaries.Count();
+            foreach (var summary in driverTripSummaries.Select((x, i) => new { Value = x, Index = i }))
             {
                 // Problem specified "to nearest int" so I'm using Convert.ToInt32 because it rounds and converts double to int in one step.
                 // If this were real life, you better believe I'd be asking someone who cared which way to round these. 
                 // Manual says:
                 // "rounded to the nearest 32-bit signed integer. If value is halfway between two whole numbers, 
                 // the even number is returned; that is, 4.5 is converted to 4, and 5.5 is converted to 6."
-                var miles = Convert.ToInt32(summary.TotalDistance);
-                var mph = Convert.ToInt32(summary.TotalDistance / summary.TotalMinutes);
+                var miles = Convert.ToInt32(summary.Value.TotalDistance);
+                var mph = Convert.ToInt32(summary.Value.TotalDistance / (summary.Value.TotalMinutes/60M));
 
-                sb.AppendFormat("{0}: {1} miles @ {2} mph", summary.DriverName, miles, mph);
+                sb.AppendFormat("{0}: {1} miles @ {2} mph", summary.Value.DriverName, miles, mph);
+
+                //new lines except at the end
+                if (summary.Index < count-1)
+                {
+                    sb.AppendLine();
+                }
             }
 
             return sb.ToString();
--- Tests/TestSuite1.cs
+++ Tests/TestSuite1.cs
@@ -22,7 +22,47 @@ namespace Tests
                 MilesDriven = 40
             });
 
-            Assert.AreEqual("HumanBeing: 40 miles @ 1 mph", Services.GenerateReport(trips));
+            Assert.AreEqual("HumanBeing: 40 miles @ 40 mph", Services.GenerateReport(trips));
         }
+
+        [TestMethod]
+        public void TestReportSortsWithHardcodedData()
+        {
+            // Trip Dan 07:15 07:45 17.3
+            // Trip Dan 06:12 06:32 21.8
+            // Trip Lauren 12:01 13:16 42.0
+            
+            var trips = new List<Trip>();
+            trips.Add(new Trip()
+            {
+                DriverName = "Dan",
+                StartTime = new DateTime(2019, 1, 1, 7, 15, 0),
+                EndTime = new DateTime(2019, 1, 1, 7, 45, 0),
+                MilesDriven = 17.3M
+            });
+            trips.Add(new Trip()
+            {
+                DriverName = "Dan",
+                StartTime = new DateTime(2019, 1, 1, 6, 12, 0),
+                EndTime = new DateTime(2019, 1, 1, 6, 32, 0),
+                MilesDriven = 21.8M
+            });
+            trips.Add(new Trip()
+            {
+                DriverName = "Lauren",
+                StartTime = new DateTime(2019, 1, 1, 12, 01, 0),
+                EndTime = new DateTime(2019, 1, 1, 13, 16, 0),
+                MilesDriven = 42
+            });
+
+            var debug = Services.GenerateReport(trips);
+            Assert.AreEqual("Lauren: 42 miles @ 34 mph\nDan: 39 miles @ 47 mph", Services.GenerateReport(trips));
+        }
+
+        //TODO make a test to handle input for ppl that aren't registered?
+        //TODO make a test to handle junk time/distance inputs
+
+        //TODO test input from text files
+        //TODO way later test 24 hr input in text file
     }
 }
